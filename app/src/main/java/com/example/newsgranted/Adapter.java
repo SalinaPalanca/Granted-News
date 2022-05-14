0543e3b03875c711f313ac2c942918890193da15
Allen <m4ap100@gmail.com>
2022-05-14 22:56:33 +0800

    Almost done

--- app/src/main/java/com/example/newsgranted/Adapter.java
+++ app/src/main/java/com/example/newsgranted/Adapter.java
@@ -1,5 +1,7 @@
 package com.example.newsgranted;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,22 +11,46 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.newsgranted.Model.Articles;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    Context context;
+    List<Articles> articles;
+
+    public Adapter (Context context, List<Articles> articles) {
+        this.context = context;
+        this.articles = articles;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        Articles a = articles.get(position);
+        holder.tvTitle.setText(a.getTitle());
+        holder.tvSource.setText(a.getSource().getName());
+        holder.tvDate.setText(a.getPublishedAt());
+
+        String imageUrl = a.getUrlToImage();
+
+        Picasso.with(context).load(imageUrl).into(holder.imageView);
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount(){
+        return articles.size();
     }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvTitle,tvSource,tvDate;
         ImageView imageView;
--- app/src/main/java/com/example/newsgranted/MainActivity.java
+++ app/src/main/java/com/example/newsgranted/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.newsgranted;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+
+import com.example.newsgranted.Model.Articles;
+import com.example.newsgranted.Model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    final String API_KEY = "6ef96dd7b2d24414b60801c38b075d68";
+    Adapter adapter;
+    List<Articles> articles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        String country = getCountry();
+        retrieveJson(country,API_KEY);
+
+    }
+
+    public void retrieveJson(String country, String apiKey) {
+
+        retrofit2.Call<Headlines> call = ApiClient.getInstance().getApi().getHeadlines(country,apiKey);
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(retrofit2.Call<Headlines> call, Response<Headlines> response) {
+                if (response.isSuccessful() && response.body().getArticles() !=null) {
+                    articles.clear();
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this, articles);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<Headlines> call, Throwable t) {
+
+            }
+        });
+    }
+
+        public String getCountry(){
+            Locale locale = Locale.getDefault();
+            String country = locale.getCountry();
+            return country.toLowerCase();
+
     }
 }
